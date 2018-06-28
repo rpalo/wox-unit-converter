f4bc8a370fce75874c44ea452c6d4e3d549ffd9e
jullit31 <jullit31@gmail.com>
2018-06-28 21:30:28 +0200

    Add message when pint is missing and instructions for users.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,45 @@
 """Wox Plugin class for unit conversions"""
-
-import pint
+import pkgutil
 from wox import Wox
+if pkgutil.find_loader("pint"): #Check for pint
+    import pint
+    ureg = pint.UnitRegistry()
+    ureg.default_format = ".5nP"
+    dependecies_exist = True
+else:
+    dependecies_exist = False
+
 
 class UnitConversion(Wox):
     """Converts units, interfaces with Wox"""
 
     def query(self, query):
-        """Returns the results of the user's search string"""
-        src, target = query.split(" to ")
-        try:
-            value = pint.UnitRegistry().Quantity(src)
-            answer = value.to(target)
-        except Exception as e:
-            answer = f"Something failed. {e}"
-        result = [{
-            "Title": f"{target.title()}",
-            "SubTitle": f"{answer}".title(),
-            "IcoPath": "Images\\app.png",
-        }]
+        """Returns instructions for the user or the results of the user's search string"""
+        result = []
+        if dependecies_exist == False:
+            result.append({
+                "Title": "Unit Converter",
+                "SubTitle": "This plugin requieres the python module 'pint'. Run 'pip install pint' to install pint.",
+                "IcoPath": "Images\\app.png"
+            })
+        elif " to " in query:
+            src, target = query.split(" to ")
+            try:
+                value = ureg.Quantity(src)
+                answer = value.to(target)
+            except Exception as e:
+                answer = f"Something failed. {e}"
+            result.append({
+                "Title": f"{target}",
+                "SubTitle": f"{answer}",
+                "IcoPath": "Images\\app.png"
+            })
+        else:
+            result.append({
+                "Title": "Unit Converter",
+                "SubTitle": "Please enter a query in the format (number) (sourceUnit) to (targetUnit)",
+                "IcoPath": "Images\\app.png"
+            })
         return result
 
 if __name__ == "__main__":
